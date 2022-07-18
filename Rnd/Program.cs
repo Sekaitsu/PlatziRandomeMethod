b9719c741faae7f0ad2bc155bc8c605bf53fef0b
SekaitsuSistecredito <lbernategordillo@gmail.com>
2022-07-18 08:06:05 -0500

    Platzi Randome Method

---
+++ Rnd/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Random random = new Random();
+
+Console.WriteLine($"{random.NextDouble()}");
+Console.WriteLine($"{random.Next()}");
+Console.WriteLine($"{random.Next()}");
+Console.WriteLine($"{random.NextDouble()}");
+Console.WriteLine($"{random.NextDouble()}");
+Console.WriteLine($"{random.NextDouble()}");
+Console.WriteLine(random);
